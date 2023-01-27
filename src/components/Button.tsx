3dda355615855bd9e801d2cbd980689bc4def59a
hwuiinn <gnldls8406@gmail.com>
2023-01-28 00:36:46 +0900

    🔥 fix : Delete interface
    - 사용되지 않는 interface를 삭제하였습니다.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import styled from "styled-components";
-import { BtnProps } from "../shared/type";
+import { BtnProps, MixBtnProps } from "../shared/type";
 
 // 버튼 크기를 props로 내려서 경우에 따라 다르게 적용하고싶은데 어떻게 해야??
 // interface 생성해서.
 
-export default function Button(props: BtnProps) {
-  console.log(props);
+export default function Button(props: MixBtnProps) {
+  console.log("props : ", props);
 
   return (
     <Container>
@@ -29,7 +29,7 @@ const Container = styled.div`
   height: 100%;
 `;
 
-const DeleteBtn = styled.div<BtnProps>`
+const DeleteBtn = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -40,15 +40,15 @@ const DeleteBtn = styled.div<BtnProps>`
   color: #262b7f;
   font-size: 14px;
   cursor: pointer;
-  transition-duration : .3s;
+  transition-duration: 0.3s;
   :hover {
     color: #f2f2f2;
-    background-color #262b7f;
+    background-color: #262b7f;
     border: 1px solid #262b7f;
-  };
+  }
 `;
 
-const EditBtn = styled.div<BtnProps>`  
+const EditBtn = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -60,10 +60,10 @@ const EditBtn = styled.div<BtnProps>`
   font-size: 14px;
   cursor: pointer;
 
-  transition-duration : .3s;
+  transition-duration: 0.3s;
   :hover {
     color: #f2f2f2;
-    background-color #262b7f;
+    background-color: #262b7f;
     border: 1px solid #262b7f;
-  };
+  }
 `;
